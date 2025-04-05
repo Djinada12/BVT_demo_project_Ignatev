dc0ffa2cf9c0dd288a621d07532a26cb34b0612d
Djinada12 <95240487+Djinada12@users.noreply.github.com>
2025-04-05 14:48:10 +0300

    Initial commit

---
+++ src/main/java/ru/mtuci/BVT_demo1/BvtDemo1Application.java
@@ -0,0 +1,13 @@
+package ru.mtuci.BVT_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvtDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvtDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mtuci/BVT_demo1/BvtDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.BVT_demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvtDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
